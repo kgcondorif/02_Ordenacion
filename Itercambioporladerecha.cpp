68cd8ec5a2d4a059d4c270f76f8f10d568c20b79
kgcondorif <kaelc3147@gmail.com>
2026-09-17 15:52:07 -0500

    Intercambio 1

---
+++ Itercambioporladerecha.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void intercambiar(int[], int);
+
+int main() {
+    int n;
+
+    cout << "Ordenacion por intercambio a la derecha" << endl;
+    cout << "Ingrese la cantidad de elementos del arreglo" << endl;
+    cin >> n;
+
+    int arreglo[n];
+
+    cout << "Ingrese los " << n << " numeros: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Numero " << i + 1 << ": ";
+        cin >> arreglo[i];
+    }
+
+    cout << "Arreglo ingresado: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl;
+
+    intercambiar(arreglo, n);
+
+    cout << "Arreglo ordenado de forma ascendente: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+void intercambiar(int arreglo[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arreglo[j] > arreglo[j + 1]) {
+                int temp = arreglo[j];
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = temp;
+            }
+        }
+    }
+}
